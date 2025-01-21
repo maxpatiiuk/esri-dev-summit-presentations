70042faceed99227d1a180bacc5782a351b312ce
Max Patiiuk <max@patii.uk>
2025-01-20 18:20:18 -0800

    perf(2025): preload images for future slides

--- 2025/.meta/global-bottom.vue
+++ 2025/.meta/global-bottom.vue
@@ -1,33 +1,32 @@
 <template>
   <img
     alt=""
+    src="https://github.com/Esri/reveal.js/blob/dev-summit-2025/img/2025/dev/3.png?raw=true"
+  />
+  <img
+    alt=""
     src="https://github.com/Esri/reveal.js/blob/dev-summit-2025/img/2025/dev/1.png?raw=true"
-    v-if="$nav.currentPage === 1"
+    v-show="$nav.currentPage === 1"
   />
   <img
     alt=""
     src="https://github.com/Esri/reveal.js/blob/dev-summit-2025/img/2025/dev/4.png?raw=true"
-    v-else-if="$nav.currentSlideRoute.meta.slide.frontmatter.is === 'intro'"
+    v-show="$nav.currentSlideRoute.meta.slide.frontmatter.is === 'intro'"
   />
   <img
     alt=""
     src="https://github.com/Esri/reveal.js/blob/dev-summit-2025/img/2025/dev/6.png?raw=true"
-    v-else-if="$nav.currentSlideRoute.meta.slide.frontmatter.is === 'feedback'"
+    v-show="$nav.currentSlideRoute.meta.slide.frontmatter.is === 'feedback'"
   />
   <img
     alt=""
     src="https://github.com/Esri/reveal.js/blob/dev-summit-2025/img/2025/dev/7.png?raw=true"
-    v-else-if="$nav.currentSlideRoute.meta.slide.frontmatter.is === 'social'"
+    v-show="$nav.currentSlideRoute.meta.slide.frontmatter.is === 'social'"
   />
   <img
     alt=""
     src="https://github.com/Esri/reveal.js/blob/dev-summit-2025/img/2025/dev/8.png?raw=true"
-    v-else-if="$nav.currentSlideRoute.meta.slide.frontmatter.is === 'esri'"
-  />
-  <img
-    alt=""
-    src="https://github.com/Esri/reveal.js/blob/dev-summit-2025/img/2025/dev/3.png?raw=true"
-    v-else
+    v-show="$nav.currentSlideRoute.meta.slide.frontmatter.is === 'esri'"
   />
 </template>
 
