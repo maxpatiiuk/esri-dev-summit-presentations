b6bb9b72075175258a5f4cb35eca161d00f942b4
maxp8108 <mpayson@esri.com>
2025-03-03 00:04:17 -0500

    feat: update demo, add chaos monkey

--- 2025/build-tooling/demo/final/src/main.tsx
+++ 2025/build-tooling/demo/final/src/main.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ApiKeyManager } from "@esri/arcgis-rest-request";
 import { App } from "./App";
 import "./index.css";
 
+const serviceInfo = {
+  authentication: ApiKeyManager.fromKey(import.meta.env.VITE_API_KEY),
+  endpoint: import.meta.env.DEV
+    ? "/arcgis/rest/services/places-service/v1/places/near-point"
+    : undefined,
+};
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <App serviceInfo={serviceInfo} />
   </React.StrictMode>,
 );
--- 2025/build-tooling/demo/final/vite.config.ts
+++ 2025/build-tooling/demo/final/vite.config.ts
@@ -1,10 +1,32 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import chaosMonkey from "./support/chaosMonkey.mjs";
+
+const API_URL = "/arcgis/rest/services/places-service";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     open: true,
+    proxy: {
+      [API_URL]: {
+        target: "https://places-api.arcgis.com",
+        changeOrigin: true,
+      },
+    },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    process.env.CHAOS
+      ? chaosMonkey([
+          {
+            apiUrl: API_URL,
+            delay: 400,
+            chaosRatio: 1,
+            // https://developers.arcgis.com/rest/places/near-point-get/#response
+            chaosErrors: [400, 401, 403, 500],
+          },
+        ])
+      : undefined,
+  ],
 });
