af677a69d35a55c3511e9bb5ab867ae1b1d34e08
Max Patiiuk <max@patii.uk>
2025-03-07 10:29:52 -0800

    docs(2025-using-components-2): improve demo description

--- 2025/using-components-2/demo/1-javascript/src/main.js
+++ 2025/using-components-2/demo/1-javascript/src/main.js
@@ -1,4 +1,8 @@
+// Importing the styles from JavaScript rather than the HTML to keep them closer
+// to the components they are styling.
 import "./styles.css";
+
+// Individually import the components we need (more efficient than the CDN loader)
 import "@esri/calcite-components/components/calcite-shell";
 import "@esri/calcite-components/components/calcite-navigation";
 import "@esri/calcite-components/components/calcite-navigation-logo";
@@ -8,15 +12,17 @@ import "@esri/calcite-components/components/calcite-chip-group";
 import "@arcgis/map-components/components/arcgis-map";
 import "@arcgis/map-components/components/arcgis-elevation-profile";
 
+// Get references to the components we need to interact with
 const elevationProfile = document.querySelector("arcgis-elevation-profile");
 const distanceElement = document.getElementById("distance");
 const elevationElement = document.getElementById("elevation");
-
 if (!elevationProfile || !distanceElement || !elevationElement) {
   throw Error("Error: Unable to find DOM elements");
 }
 
+// Watch for "progress" changes on the elevation profile component
 elevationProfile.addEventListener("arcgisPropertyChange", (event) => {
+  // 1 = complete
   if (event.detail.name !== "progress" || elevationProfile.progress !== 1) {
     return;
   }
