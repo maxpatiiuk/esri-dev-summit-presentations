ae36487ad0f5632ba0a0abdcb77847772734b59e
Nick Romano <nromano@esri.com>
2025-03-07 11:53:19 -0800

    feat(using-components-2): add react-typescript app

---
+++ 2025/using-components-2/demo/3-typescript/src/vite-env.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="vite/client" />
+/// <reference types="@arcgis/core/interfaces" />
+/// <reference types="@esri/calcite-components/dist/types/react" />
+/// <reference types="@arcgis/map-components/dist/types/react" />
