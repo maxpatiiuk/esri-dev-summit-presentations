03cbcdabcb00fdb8da4d017c0c4bcf0ace6366b2
Nick Romano <nromano@esri.com>
2025-03-07 11:53:19 -0800

    feat(using-components-2): add react-typescript app

---
+++ 2025/using-components-2/demo/3-typescript/src/App.tsx
@@ -0,0 +1,84 @@
+import React from "react";
+import "@esri/calcite-components/components/calcite-shell";
+import "@esri/calcite-components/components/calcite-navigation";
+import "@esri/calcite-components/components/calcite-navigation-logo";
+import "@esri/calcite-components/components/calcite-panel";
+import "@esri/calcite-components/components/calcite-chip";
+import "@esri/calcite-components/components/calcite-chip-group";
+import "@arcgis/map-components/components/arcgis-map";
+import "@arcgis/map-components/components/arcgis-elevation-profile";
+
+const round = (value: number) =>
+      Math.round(((value ?? 0) + Number.EPSILON) * 100) / 100;
+
+const App = () => {
+  const [ distance, setDistance ] = React.useState<undefined | string>(undefined);
+  const [ elevation, setElevation ] = React.useState<undefined | string>(undefined);
+
+  const handleElevationProfileChange = (event: HTMLArcgisElevationProfileElement['arcgisPropertyChange']) => {
+    if (event.detail.name !== "progress" || event.target.progress !== 1) {
+      return;
+    }
+    const profiles = event.target.profiles;
+    const statistics = profiles.at(0)?.statistics;
+    if(!statistics) {
+      return;
+    }
+    if(statistics.elevationGain) {
+      const elevationGain = round(statistics.elevationGain);
+      setElevation(`${elevationGain} ${event.target.effectiveUnits.elevation}`);
+    }
+    if(statistics.maxDistance) {
+      const distance = round(statistics.maxDistance);
+      setDistance(`${distance} ${event.target.effectiveUnits.distance}`);
+    }
+  }
+
+  return (
+    <calcite-shell className="custom-theme">
+      <calcite-navigation slot="header">
+        <calcite-navigation-logo
+          slot="logo"
+          icon="map"
+          heading="US National Parks"
+          description="Trail Explorer"
+        ></calcite-navigation-logo>
+      </calcite-navigation>
+
+      <div className="content-container">
+        <div className="content-start">
+          <calcite-panel id="map-container">
+            <arcgis-map
+              ground="world-elevation"
+              item-id="5fe7222cfd4e41cab4321cc1fde66cc2"
+              id="map"
+            ></arcgis-map>
+            <calcite-panel
+              id="elevation-panel"
+              heading="Elevation profile"
+            >
+              <calcite-chip-group slot="header-actions-end" label="Statistics">
+                <calcite-chip
+                  icon="walking-distance"
+                  id="distance"
+                  label="Walking Distance"
+                >{distance}</calcite-chip>
+                <calcite-chip icon="altitude" id="elevation" label="Elevation">{elevation}</calcite-chip>
+              </calcite-chip-group>
+              <arcgis-elevation-profile
+                reference-element="map"
+                unit="imperial"
+                hide-clear-button
+                hide-legend
+                hide-settings-button
+                onarcgisPropertyChange={handleElevationProfileChange}
+              ></arcgis-elevation-profile>
+            </calcite-panel>
+          </calcite-panel>
+        </div>
+      </div>
+    </calcite-shell>
+  );
+};
+
+export default App;
