94186f7c0ae3fa56b684569e695a7ebc4d782be5
Max Patiiuk <max@patii.uk>
2025-03-10 11:20:02 -0700

    ci(monorepo): add global ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import eslintConfigLumina from '@arcgis/eslint-config/lumina';
 
 export default [
   {
-    ignores: ['dist', 'node_modules', '**/*.js'],
+    ignores: ['dist', 'node_modules', '**/*.js', '**/*.cjs'],
   },
   ...eslintConfig,
   ...eslintConfigLumina,
@@ -12,6 +12,15 @@ export default [
       parserOptions: {
         projectService: true,
       },
+    },
+    rules: {
+      // Soften rules to make demos simpler
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-magic-numbers': 'off',
+      '@typescript-eslint/no-floating-promises': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
     },
   },
 ];
