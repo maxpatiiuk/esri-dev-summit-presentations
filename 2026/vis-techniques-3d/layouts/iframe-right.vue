b3b166c553eb63cab21dddf707bbc78527ea8896
Thorben Westerhuys <twesterhuys@esri.com>
2026-02-13 15:49:37 +0100

    feat(2026-vis-techniques-3d): custom iframe and image components

---
+++ 2026/vis-techniques-3d/layouts/iframe-right.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+type Props = {
+  url: string;
+  extendedIframe?: boolean;
+  scale?: number;
+};
+
+defineProps<Props>();
+</script>
+
+<template>
+  <div class="slidev-layout iframe-right h-full w-full grid grid-cols-2 gap-10">
+    <div>
+      <slot />
+    </div>
+
+    <div class="h-full mt-8">
+      <IframeScreen
+        :url="url"
+        :scale="scale"
+        class="h-full"
+      />
+    </div>
+  </div>
+</template>
