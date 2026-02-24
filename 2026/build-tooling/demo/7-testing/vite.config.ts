eee8f4e77410f6abf899b285d37ad7e28f21e2f7
Hugo Campos <hcampos@esri.com>
2026-02-24 09:45:27 +0100

    chore: update dependencies

--- 2026/build-tooling/demo/7-testing/vite.config.ts
+++ 2026/build-tooling/demo/7-testing/vite.config.ts
@@ -1,5 +1,5 @@
 /// <reference types="vitest" />
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import { playwright } from "@vitest/browser-playwright";
 import { defineConfig } from "vitest/config";
 
--- 2026/build-tooling/demo/final/vite.config.ts
+++ 2026/build-tooling/demo/final/vite.config.ts
@@ -1,6 +1,6 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import { playwright } from "@vitest/browser-playwright";
 import chaosMonkey from "./support/chaosMonkey.js";
 
