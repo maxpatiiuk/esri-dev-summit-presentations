f6365892eb398e161da8e6563dbaf85fc2102435
Nick Romano <nromano@esri.com>
2026-02-25 10:24:24 -0800

    refactor: update javascript app to use new demo

--- 2026/using-components-2/demo/1-javascript/src/main.js
+++ 2026/using-components-2/demo/1-javascript/src/main.js
@@ -1,6 +1,6 @@
 // Importing the styles from JavaScript rather than the HTML to keep them closer
 // to the components they are styling.
-import "./styles.css";
+import "./style.css";
 
 // Individually import the components we need (more efficient than the CDN loader)
 import "@esri/calcite-components/components/calcite-shell";
@@ -11,28 +11,120 @@ import "@esri/calcite-components/components/calcite-chip";
 import "@esri/calcite-components/components/calcite-chip-group";
 import "@arcgis/map-components/components/arcgis-map";
 import "@arcgis/map-components/components/arcgis-elevation-profile";
+import "@arcgis/map-components/components/arcgis-feature-table";
+import "@arcgis/map-components/components/arcgis-search";
+import "@arcgis/map-components/components/arcgis-popup";
+import "@arcgis/map-components/components/arcgis-legend";
 
-// Get references to the components we need to interact with
-const elevationProfile = document.querySelector("arcgis-elevation-profile");
-const distanceElement = document.getElementById("distance");
-const elevationElement = document.getElementById("elevation");
-if (!elevationProfile || !distanceElement || !elevationElement) {
-  throw Error("Error: Unable to find DOM elements");
+import * as reactiveUtils from "@arcgis/core/core/reactiveUtils.js";
+
+async function load() {
+  const mapElem = document.querySelector("arcgis-map");
+  const tableElement = document.querySelector("arcgis-feature-table");
+  const searchElement = document.querySelector("arcgis-search");
+  const elevProf = document.querySelector("arcgis-elevation-profile");
+  const distanceElem = document.getElementById("distance");
+  const elevationElem = document.getElementById("elevation");
+  const popupElement = document.querySelector("arcgis-popup");
+
+  popupElement.dockOptions = {
+    buttonEnabled: false,
+    breakpoint: false,
+    position: "top-right",
+  };
+
+  await mapElem.viewOnReady();
+  const view = mapElem.view;
+  await elevProf.componentOnReady();
+
+  // set the highlight colors
+  mapElem.highlights = [
+    { name: "default", color: "#FF7500" },
+    { name: "temporary", color: "#a2f2a4" },
+  ];
+
+  //////////////////////////////////////////////////////
+  //  Table setup
+  //////////////////////////////////////////////////////
+
+  const trailsLayer = mapElem.map?.layers.find((layer) => {
+    console.log(layer.title);
+    return layer.title === "National Park Trails";
+  });
+
+  tableElement.layer = trailsLayer;
+  tableElement.multipleSelectionDisabled = true;
+
+  mapElem.addEventListener("arcgisViewChange", () => {
+    tableElement.filterGeometry = mapElem.view.extent;
+  });
+
+  //////////////////////////////////////////////////////
+  //  Handle selection
+  //////////////////////////////////////////////////////
+
+  searchElement.addEventListener("arcgisSearchComplete", (event) => {
+    const result = event.detail.results[0].results[0];
+    if (result?.feature) {
+      const objectId = result.feature.getObjectId();
+      view.selectionManager.replace(trailsLayer, [objectId]);
+    }
+  });
+
+  mapElem.addEventListener("arcgisViewClick", async (event) => {
+    const { results } = await view.hitTest(event.detail, {
+      include: [trailsLayer],
+    });
+    if (results?.length > 0) {
+      const objectId = results[0].graphic.getObjectId();
+      view.selectionManager.replace(trailsLayer, [objectId]);
+    }
+  });
+
+  view.selectionManager.on("selection-change", async (event) => {
+    const results = await view.selectionManager.getSelectedFeatures(
+      [trailsLayer],
+      { returnGeometry: true },
+    );
+
+    if (results.length && results[0].data.features.length) {
+      const graphic = results[0].data.features[0];
+      elevProf.feature = graphic;
+      mapElem.openPopup({
+        features: [graphic],
+      });
+    }
+  });
+
+  //////////////////////////////////////////////////////
+  //  Wire up the elevation profile
+  //////////////////////////////////////////////////////
+
+  await elevProf.componentOnReady();
+
+  const analysisView = await view.whenAnalysisView(elevProf.analysis);
+  reactiveUtils.watch(
+    () => analysisView.progress,
+    (progress) => {
+      if (progress === 1) {
+        console.log("Results are computed", analysisView.results);
+        console.log("Statistics are computed", analysisView.statistics);
+
+        const elevGain =
+          Math.round(
+            (analysisView.statistics.elevationGain + Number.EPSILON) * 100,
+          ) / 100;
+        const distance =
+          Math.round(
+            (analysisView.statistics.maxDistance + Number.EPSILON) * 100,
+          ) / 100;
+        elevationElem.innerText =
+          elevGain + " " + elevProf.effectiveDisplayUnits.elevation;
+        distanceElem.innerText =
+          distance + " " + elevProf.effectiveDisplayUnits.distance;
+      }
+    },
+  );
 }
 
-// Watch for "progress" changes on the elevation profile component
-elevationProfile.addEventListener("arcgisPropertyChange", (event) => {
-  // 1 = complete
-  if (event.detail.name !== "progress" || elevationProfile.progress !== 1) {
-    return;
-  }
-  const profiles = elevationProfile.profiles;
-  const statistics = profiles.at(0)?.statistics;
-  const elevationGain = round(statistics?.elevationGain);
-  const distance = round(statistics?.maxDistance);
-  elevationElement.textContent = `${elevationGain} ${elevationProfile.effectiveUnits.elevation}`;
-  distanceElement.textContent = `${distance} ${elevationProfile.effectiveUnits.distance}`;
-});
-
-const round = (value) =>
-  Math.round(((value ?? 0) + Number.EPSILON) * 100) / 100;
+load();
